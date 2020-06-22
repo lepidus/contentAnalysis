f524b462c91bd07d232e16d76e51ea2b57ed7784
JhonathanMiranda <jhon@lepidus.com.br>
2020-06-22 12:14:52 -0400

    Inicializando o repositório do plugin
    Signed-off-by: JhonathanMiranda <jhon@lepidus.com.br>

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @defgroup plugins_generic_documentMetadataChecklist
+ */
+/**
+ * @file plugins/generic/documentMetadataChecklist/index.php
+ *
+ * @ingroup plugins_generic_documentMetadataChecklist
+ * @brief Wrapper for the Document Metadata Checklist plugin.
+ *
+ */
+require_once('DocumentMetadataChecklistPlugin.inc.php');
+return new DocumentMetadataChecklistPlugin();
