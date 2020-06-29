fbe2e61a1077781e0f796409c73d0645966b1cca
JhonathanMiranda <jhon@lepidus.com.br>
2020-06-29 18:22:39 -0400

    Adicionando primeiros testes para grupos de preprints
    Estes testes visam aumentar a acurácia na detecção de casos positivos e negativos
    
    Signed-off-by: JhonathanMiranda <jhon@lepidus.com.br>

---
+++ tests/FirstPercentageTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class FirstPercentageTest extends TestCase {
+
+    public function testPercentagePositives() : void {
+        $mainPath = "/home/jhon/Documentos/amostra_preprints/positives/";
+        $countPositives = 0;
+
+        for($i = 1; $i <= 5; $i++){
+            $path = $mainPath . $i . ".pdf";
+            $checker = new DocumentChecker($path);
+
+            if($checker->checkAuthorsContribution())
+                $countPositives++;
+        }
+
+        $this->assertGreaterThanOrEqual(4, $countPositives);
+    }
+
+    public function testPercentageNegatives() : void {
+        $mainPath = "/home/jhon/Documentos/amostra_preprints/negatives/";
+        $countNegatives = 0;
+
+        for($i = 1; $i <= 5; $i++){
+            $path = $mainPath . $i . ".pdf";
+            $checker = new DocumentChecker($path);
+
+            if(!$checker->checkAuthorsContribution())
+                $countNegatives++;
+        }
+
+        $this->assertEquals(5, $countNegatives);
+    }
+}
