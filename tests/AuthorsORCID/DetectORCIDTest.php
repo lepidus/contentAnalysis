a39bd9178cbca25e646a26b894a7e25dc72580ea
JhonathanMiranda <jhon@lepidus.com.br>
2020-07-08 14:07:50 -0400

    Adiciona os testes da detecção de ORCID
    Também reorganiza o diretório dos testes
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

---
+++ tests/AuthorsORCID/DetectORCIDTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class DetectORCIDTest extends TestCase {
+    
+    public function testORCIDPresent() : void {
+        $pathDocument = "/home/jhon/Documentos/base_preprints/positivos/1.pdf";
+        $checker = new DocumentChecker($pathDocument);
+
+        $this->assertTrue($checker->checkAuthorsORCID());
+    }
+
+    public function testORCIDNotPresent() : void{
+        $pathDocument = "/home/jhon/Documentos/base_preprints/negativos/1.pdf";
+        $checker = new DocumentChecker($pathDocument);
+
+        $this->assertFalse($checker->checkAuthorsORCID());
+    }
+}
