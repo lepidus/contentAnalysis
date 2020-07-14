b4cb6ebec0e18543ca7426829415e91efca2de78
JhonathanMiranda <jhon@lepidus.com.br>
2020-07-14 16:12:33 -0400

    Adicionando testes em massa para a detecção de conflito de interesses
    Issue documentacao-e-tarefas/scielo#57
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

---
+++ tests/ConflictInterest/PercentageTest.php
@@ -0,0 +1,42 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class PercentageTest extends TestCase {
+
+    public function testPercentagePositives() : void {
+        $mainPath = "/home/jhon/Documentos/base_preprints/positivos/";
+        $foundPositives = 0;
+
+        for($i = 1; $i <= 72; $i++){
+            $path = $mainPath . $i . ".pdf";
+            $checker = new DocumentChecker($path);
+            
+            if($checker->checkConflictInterest())
+                $foundPositives++;
+        }
+
+        $accuracy = ($foundPositives / 72);
+
+        $this->assertGreaterThanOrEqual(0.85, $accuracy);
+    }
+
+    public function testPercentageNegatives() : void {
+        $mainPath = "/home/jhon/Documentos/base_preprints/negativos/";
+        $foundNegatives = 0;
+
+        for($i = 1; $i <= 123; $i++){
+            $path = $mainPath . $i . ".pdf";
+            $checker = new DocumentChecker($path);
+
+            if(!$checker->checkConflictInterest())
+                $foundNegatives++;
+            else
+                error_log($path);
+        }
+
+        $accuracy = ($foundNegatives / 123);
+
+        $this->assertEquals(1, $accuracy);
+    }
+}
