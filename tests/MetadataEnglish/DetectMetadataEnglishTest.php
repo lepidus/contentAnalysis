a33119010aaeedfb0889843b78f77737e8f8b941
Jhon <jhon@lepidus.com.br>
2020-09-28 17:08:00 -0400

    Alteração nos testes
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- tests/MetadataEnglish/DetectMetadataEnglishTest.php
+++ tests/MetadataEnglish/DetectMetadataEnglishTest.php
@@ -8,13 +8,13 @@ final class DetectMetadataEnglishTest extends TestCase {
         $pathDocument = "/home/jhon/Documentos/base_metadados_ingles/positivos/1.pdf";
         $checker = new DocumentChecker($pathDocument);
 
-        $this->assertEquals('Success', $checker->checkMetadataInEnglish()[0]);
+        $this->assertEquals('Success', $checker->checkMetadataInEnglish("Coping with stress in pandemic times: A booklet proposal")['statusMetadataEnglish']);
     }
 
     public function testMetadataEnglishNotPresent() : void{
         $pathDocument = "/home/jhon/Documentos/base_metadados_ingles/negativos/1.pdf";
         $checker = new DocumentChecker($pathDocument);
 
-        $this->assertEquals('Error', $checker->checkMetadataInEnglish()[0]);
+        $this->assertEquals('Error', $checker->checkMetadataInEnglish("")['statusMetadataEnglish']);
     }
 }
