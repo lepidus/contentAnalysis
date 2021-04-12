ae9b429bac46c3477f708159e0e5bb952f2552d1
Yves Amorim <yves@lepidus.com.br>
2021-04-12 14:58:16 -0400

    Adds LICENSE and updates references.
    Signed-off-by: Yves <yves@lepidus.com.br>
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- ContentAnalysisPlugin.inc.php
+++ ContentAnalysisPlugin.inc.php
@@ -5,6 +5,10 @@
  * @class ContentAnalysis
  * @ingroup plugins_generic_contentAnalysis
  *
+ * Copyright (c) 2019-2021 Lepidus Tecnologia
+ * Copyright (c) 2020-2021 SciELO
+ * Distributed under the GNU GPL v3. For full terms see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt
+ * 
  * @brief Plugin class for the Content Analysis plugin.
  */
 import('lib.pkp.classes.plugins.GenericPlugin');
--- index.php
+++ index.php
@@ -5,6 +5,10 @@
 /**
  * @file plugins/generic/contentAnalysis/index.php
  *
+ * Copyright (c) 2019-2021 Lepidus Tecnologia
+ * Copyright (c) 2020-2021 SciELO
+ * Distributed under the GNU GPL v3. For full terms see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt
+ * 
  * @ingroup plugins_generic_contentAnalysis
  * @brief Wrapper for the Content Analysis plugin.
  *
