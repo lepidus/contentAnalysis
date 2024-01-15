b9fa1bb5a406aa8c7774e920975038f0882d11fe
Jhon <jhon@lepidus.com.br>
2024-01-15 17:41:51 -0400

    Fixes error in cypress test
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- cypress/tests/Test1_standardChecklist.cy.js
+++ cypress/tests/Test1_standardChecklist.cy.js
@@ -42,7 +42,7 @@ describe('Content Analysis Plugin - Standard checklist execution', function() {
         cy.reload();
 
         cy.assertCheckingsFailed(submissionData.title, 'standard');
-        cy.contains('Theresdf are one or more problems that need to be fixed before you can submit.');
+        cy.contains('There are one or more problems that need to be fixed before you can submit.');
         cy.contains('button', 'Submit').should('be.disabled');
     });
     it('Authors contribution statement checking is skipped on single author submissions', function () {
