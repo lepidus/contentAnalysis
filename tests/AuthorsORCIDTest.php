3e78e4405f9420437719baadb4f99fe5f192df20
Jhon <jhon@lepidus.com.br>
2024-03-22 18:43:00 -0400

    Adds unit tests for ORCIDs with line breaks
    Issue: documentacao-e-tarefas/scielo#647
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- tests/AuthorsORCIDTest.php
+++ tests/AuthorsORCIDTest.php
@@ -8,7 +8,7 @@ class AuthorsORCIDTest extends DetectionOnDocumentTest
     private $validTextOrcids = [
         "https://orcid.org/0000-0001-5727-2427",
         "https://orcid.org/0000-0002-1648-966X",
-        "orcid.org/0000-0001-5727-2427"
+        "orcid.org/0000-0002-1825-0097"
     ];
     private $invalidTextOrcids = [
         "0000-0001-5727-2427",
@@ -35,7 +35,23 @@ class AuthorsORCIDTest extends DetectionOnDocumentTest
         $this->assertEquals(3, $this->documentChecker->checkTextOrcidsNumber());
     }
 
-    public function doesntDetectInvalidTextOrcids(): void
+    public function testDetectsOrcidsWithLineBreaks(): void
+    {
+        $orcidWithLineBreak = ['https://orcid.org/0000-0002-', '9826-4690'];
+        $this->documentChecker->words = $this->insertWordsIntoDocWordList($orcidWithLineBreak, $this->documentChecker->words);
+
+        $this->assertEquals(1, $this->documentChecker->checkTextOrcidsNumber());
+    }
+
+    public function testDoesntDetectsRepeatedOrcids(): void
+    {
+        $repeatedOrcid = ['https://orcid.org/0000-0001-5727-2427', 'https://orcid.org/0000-0001-5727-2427'];
+        $this->documentChecker->words = $this->insertWordsIntoDocWordList($repeatedOrcid, $this->documentChecker->words);
+
+        $this->assertEquals(1, $this->documentChecker->checkTextOrcidsNumber());
+    }
+
+    public function testDoesntDetectInvalidTextOrcids(): void
     {
         $this->documentChecker->words = $this->insertWordsIntoDocWordList($this->invalidTextOrcids, $this->documentChecker->words);
 
