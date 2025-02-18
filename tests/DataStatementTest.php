60714bda35eae2c9d532eaf6c1a5d3ad62496df5
Jhon <jhon@lepidus.com.br>
2025-02-18 18:22:40 -0400

    Adiciona testes de unidade para declaração de dados
    Issue: documentacao-e-tarefas/scielo#746
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

---
+++ tests/DataStatementTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require_once("DetectionOnDocumentTest.php");
+
+class DataStatementTest extends DetectionOnDocumentTest
+{
+    private $patternsDataStatement = [
+        ["data", "statement"],
+        ["data", "availability", "statement"],
+        ["dados", "de", "pesquisa"],
+        ["datos", "de", "investigación"]
+    ];
+
+    public function testDetectsDataStatement(): void
+    {
+        $documentWords = $this->documentChecker->words;
+
+        foreach ($this->patternsDataStatement as $pattern) {
+            $this->documentChecker->words = $this->insertWordsIntoDocWordList($pattern, $documentWords);
+            $this->assertEquals("Success", $this->documentChecker->checkDataStatement());
+        }
+    }
+
+    public function testDoesntDetectDataStatementWhenNotPresent(): void
+    {
+        $this->assertEquals("Error", $this->documentChecker->checkDataStatement());
+    }
+}
