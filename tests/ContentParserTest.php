a027785c92382912fa752f05066ffdc873f850ed
Jhon <jhon@lepidus.com.br>
2025-09-17 11:47:07 -0400

    Refactors unit tests, adding unit tests for content parser
    Issue: documentacao-e-tarefas/scielo#832
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

---
+++ tests/ContentParserTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace APP\plugins\generic\contentAnalysis\tests;
+
+use PHPUnit\Framework\TestCase;
+use APP\plugins\generic\contentAnalysis\classes\ContentParser;
+
+class ContentParserTest extends TestCase
+{
+    private const FIXTURES_PATH = __DIR__ . DIRECTORY_SEPARATOR . 'fixtures' . DIRECTORY_SEPARATOR;
+    private $contentParser;
+
+    public function setUp(): void
+    {
+        $this->contentParser = new ContentParser();
+    }
+
+    public function testParsingOfDocument(): void
+    {
+        $expectedParsedWords = [
+            'lorem', 'ipsum', 'dolor', 'sit', 'amet,', 'consectetur', 'adipiscing', 'elit.', 'proin', 'arcu', 'diam,',
+            'elementum', 'id', 'quam', 'id,', 'finibus', 'porttitor', 'dolor.', 'donec', 'porta', 'ullamcorper',
+            'volutpat.'
+        ];
+        $parsedWords = $this->contentParser->parseDocument(self::FIXTURES_PATH . 'dummy_document.pdf');
+        $parsedWords = array_slice($parsedWords, 0, count($expectedParsedWords));
+
+        $this->assertEquals($expectedParsedWords, $parsedWords);
+    }
+
+    public function testParsingOfLineNumberedDocument(): void
+    {
+        $expectedParsedWords = [
+            'lorem', 'ipsum', 'dolor', 'sit', 'amet,', 'consectetur', 'adipiscing', 'elit.',
+            'etiam', 'ex', 'libero,', 'porttitor', 'a', 'elit', 'eget,', 'maximus', 'viverra', 'arcu.'
+        ];
+        $parsedWords = $this->contentParser->parseDocument(self::FIXTURES_PATH . 'dummy_document_numbered.pdf');
+        $parsedWords = array_slice($parsedWords, 0, count($expectedParsedWords));
+
+        $this->assertEquals($expectedParsedWords, $parsedWords);
+    }
+}
